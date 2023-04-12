a9024b0be6a9188cf2772e29b3a16bc597c14da4
delay100 <qor3127@naver.com>
2023-04-12 16:47:25 +0900

    add naver oauth2 login

--- springsecurity/jwt/security1/security1/src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
+++ springsecurity/jwt/security1/security1/src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -3,14 +3,18 @@ package com.cos.security1.config.oauth;
 import com.cos.security1.Repository.UserRepository;
 import com.cos.security1.config.CustomBCryptPasswordEncoder;
 import com.cos.security1.config.auth.PrincipleDetails;
+import com.cos.security1.config.oauth.provider.GoogleUserInfo;
+import com.cos.security1.config.oauth.provider.NaverUserInfo;
+import com.cos.security1.config.oauth.provider.OAuth2UserInfo;
 import com.cos.security1.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
@@ -36,18 +40,38 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         System.out.println("getAttributes: "+ oAuth2User.getAttributes()); //super.loadUser(userRequest)가 oAuth2User와 동일
 
 
+        OAuth2UserInfo oAuth2UserInfo = null;
+        if(userRequest.getClientRegistration().getRegistrationId().equals("google")){
+            System.out.println("google login/구글 로그인 요청");
+            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+        } else if(userRequest.getClientRegistration().getRegistrationId().equals("naver")){
+            System.out.println("naver login / 네이버 로그인 요청");
+            oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
+            System.out.println(oAuth2UserInfo);
+        }
+        else{
+            System.out.println("we only google, naver/우리는 구글, 네이버만 지원해용~");
+        }
+
         // 회원가입을 강제로 진행할 것
-        String provider = userRequest.getClientRegistration().getClientId();
-        String providerId = oAuth2User.getAttribute("sub");
+//        String provider = userRequest.getClientRegistration().getRegistrationId();
+//        String providerId = oAuth2User.getAttribute("sub");
+//        String username = provider+"_"+providerId; // google_1239103213
+//        String password = customBCryptPasswordEncoder.encode("겟인데어");
+//        String email = oAuth2User.getAttribute("email");
+//        String role = "ROLE_USER";
+        String provider = oAuth2UserInfo.getProvider();
+        String providerId = oAuth2UserInfo.getProviderId();
         String username = provider+"_"+providerId; // google_1239103213
         String password = customBCryptPasswordEncoder.encode("겟인데어");
-        String email = oAuth2User.getAttribute("email");
+        String email = oAuth2UserInfo.getEmail();
         String role = "ROLE_USER";
 
+        System.out.println("provider: "+provider+" providerId: "+providerId+" username: "+username+" password: "+password+" email: "+email);
         User userEntity = userRepository.findByUsername(username);
 
         if(userEntity == null) {
-            System.out.println("로그인이 처음입니다.");
+            System.out.println("oauth login first/ OAuth 로그인이 처음입니다.");
             userEntity = User.builder()
                     .username(username)
                     .password(password)
@@ -59,7 +83,7 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
             userRepository.save(userEntity);
         }
         else{
-            System.out.println("구글로그인을 이미 한 적이 있습니다. 당신은 자동 회원가입이 되어있습니다.");
+            System.out.println("oauth login already/ OAuth 로그인을 이미 한 적이 있습니다. 당신은 자동 회원가입이 되어있습니다.");
         }
 //        return super.loadUser(userRequest);
         return new PrincipleDetails(userEntity, oAuth2User.getAttributes());
