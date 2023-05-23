2919884a2c4cb1711fd85e6bc9ba48385d5b99e3
delay100 <qor3127@naver.com>
2023-05-23 11:18:54 +0900

    add week12 Spring 실습(6) - 도서관리 예제2(crud)

--- dbP/week11/book-mybatis/src/main/java/com/example/bookmybatis/service/BookService.java
+++ dbP/week11/book-mybatis/src/main/java/com/example/bookmybatis/service/BookService.java
@@ -30,4 +30,53 @@ public class BookService {
         }
         return list;
     }
+
+    public Long addBook(Book.Create bookForm) {
+        BookEntity bookEntity = new BookEntity();
+        bookEntity.setName(bookForm.getName());
+        bookEntity.setPublisher(bookForm.getPublisher());
+        bookEntity.setPrice(bookForm.getPrice());
+        bookRepository.save(bookEntity);
+        return bookEntity.getId();
+    }
+
+    public List<Book.Simple> findCondBooks(Book.Create bookForm) {
+        BookEntity bookEntity = new BookEntity();
+        bookEntity.setName(bookForm.getName());
+        bookEntity.setPublisher(bookForm.getPublisher());
+        bookEntity.setPrice(bookForm.getPrice());
+
+        List<Book.Simple> list  = new ArrayList<>();
+        for (BookEntity bookEntity2 : bookRepository.findCond(bookEntity)){
+            Book.Simple book2 = new Book.Simple();
+            book2.setId(bookEntity2.getId());
+            book2.setName(bookEntity2.getName());
+            book2.setPublisher(bookEntity2.getPublisher());
+            book2.setPrice(bookEntity2.getPrice());
+            list.add(book2);
+        }
+        return list;
+    }
+
+
+    public BookEntity getBookById(Long bookId ){
+        return bookRepository.findById(bookId).orElseThrow(IllegalArgumentException::new);
+    }
+
+    public void updateBookService(Long bookId, Book.Update updateForm){
+        BookEntity bookEntity = bookRepository.findById(bookId).orElseThrow(IllegalArgumentException::new);
+
+        bookEntity.setName(updateForm.getName());
+        bookEntity.setPublisher(updateForm.getPublisher());
+        bookEntity.setPrice(updateForm.getPrice());
+        bookRepository.update(bookEntity);
+    }
+
+    public void deleteBook(Long bookId) {
+        BookEntity bookEntity = bookRepository.findById(bookId).orElseThrow(IllegalArgumentException::new);
+
+        bookRepository.delete(bookEntity);
+
+    }
+
 }
